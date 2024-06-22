d93a7748956cf78480a954308cb50f27a2c5e47b
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-22 16:34:23 +0200

    feat: add form

--- frontend/src/components/prompt.tsx
+++ frontend/src/components/prompt.tsx
@@ -1,22 +1,49 @@
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
+import { Form, FormField } from '@/components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
-type Props = {
-	handleOnClick: () => void;
-};
+const PromptSchema = z.object({
+	user_input: z.string().min(1, {
+		message: 'Prompt cannot be empty',
+	}),
+});
 
-export default function Prompt({ handleOnClick }: Props) {
+export default function Prompt() {
+	const promptForm = useForm<z.infer<typeof PromptSchema>>({
+		resolver: zodResolver(PromptSchema),
+	});
+
+	function onSubmit(data: z.infer<typeof PromptSchema>) {
+		console.log(data);
+	}
 	return (
-		<div className='flex justify-between'>
-			<Textarea className='w-3/4 h-[100px]' placeholder='Enter your prompt' />
-			<Button
-				type='submit'
-				variant='destructive'
-				className='w-1/5 h-[100px] font-bold'
-				onClick={handleOnClick}
+		<Form {...promptForm}>
+			<form
+				onSubmit={promptForm.handleSubmit(onSubmit)}
+				className='flex justify-between'
 			>
-				Send
-			</Button>
-		</div>
+				<FormField
+					name='user_input'
+					render={({ field }) => (
+						<Textarea
+							className='w-3/4 h-[100px]'
+							placeholder='Enter your prompt'
+							{...field}
+						/>
+					)}
+				/>
+				<Button
+					type='submit'
+					variant='destructive'
+					className='w-1/5 h-[100px] font-bold'
+					// onClick={handleOnClick}
+				>
+					Send
+				</Button>
+			</form>
+		</Form>
 	);
 }
