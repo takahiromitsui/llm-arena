59f24764c2d537557f04298cc39baabd14ae1923
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-23 13:28:54 +0200

    style: split screen evenly

--- frontend/src/components/model-placeholder.tsx
+++ frontend/src/components/model-placeholder.tsx
@@ -3,25 +3,6 @@ type Props = {
 	content: string;
 };
 
-// export default function ModelPlaceholder({ modelA, modelB }: Props) {
-// 	return (
-// 		<div className='flex bg-zinc-900 border border-slate-700 rounded-md py-2 h-[500px] text-slate-100'>
-// 			<div className='w-1/2'>
-// 				<label className='p-2 bg-primary border border-slate-700 rounded-md bg-scroll'>
-// 					Model A
-// 				</label>
-// 				<div className='pt-4 pl-2 overflow-auto  h-[450px]'>{modelA}</div>
-// 			</div>
-// 			<div className='w-1/2 border-l border-slate-700'>
-// 				<label className='p-2 bg-primary border border-slate-700 rounded-md bg-scroll'>
-// 					Model B
-// 				</label>
-// 				<div className='pt-4 pl-2 overflow-auto  h-[450px]'>{modelB}</div>
-// 			</div>
-// 		</div>
-// 	);
-// }
-
 export default function ModelPlaceholder({ label, content }: Props) {
 	return (
 		<div className='w-1/2'>
