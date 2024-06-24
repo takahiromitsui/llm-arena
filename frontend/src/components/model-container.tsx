e13abe096b860d8dc62917be7157125111ed6a7c
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-06-24 08:01:14 +0200

    chore: add tanstack query

--- frontend/src/components/model-container.tsx
+++ frontend/src/components/model-container.tsx
@@ -9,10 +9,8 @@ export default function ModelContainer({ modelA, modelB }: Props) {
 	return (
 		<div className='flex bg-zinc-900 border border-slate-700 rounded-md py-2 h-[500px] text-slate-100'>
 			<ModelPlaceholder label='Model A' content={modelA} />
-			{/* <div className='border-l border-slate-700'> */}
 			<div className='border-l border-slate-700'></div>
 			<ModelPlaceholder label='Model B' content={modelB} />
-			{/* </div> */}
 		</div>
 	);
 }
